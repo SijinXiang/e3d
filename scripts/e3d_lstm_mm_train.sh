9a1ba99e8b4cd47f0c29d1fb4d6712184b1671c0
SijinXiang <xiangsijin@hotmail.com>
2019-08-02 20:13:58 +0000

    backup for changes on 8.2

--- scripts/e3d_lstm_mm_train.sh
+++ scripts/e3d_lstm_mm_train.sh
@@ -15,29 +15,29 @@
 
 cd ..
 python run.py \
-    --train_data_paths ../data/milan_tra.npy \
-    --valid_data_paths ../data/milan_val.npy \
-    --test_data_paths ../data/milan_test.npy \
-    --save_dir checkpoints/_mnist_e3d_lstm \
-    --gen_frm_dir results/_mnist_e3d_lstm \
+    --train_data_paths data/milan_tra.npy \
+    --valid_data_paths data/milan_val.npy \
+    --test_data_paths data/milan_test.npy \
+    --save_dir checkpoints/8-2-2/8-8-4 \
+    --gen_frm_dir results/8-2-2/8-8-4 \
     --is_training False \
     --dataset_name milan \
-    --input_seq_length 4 \
-    --output_seq_length 4 \
-    --dimension_3D 1 \
+    --input_seq_length 8 \
+    --output_seq_length 2 \
+    --dimension_3D 2 \
     --img_width 100 \
     --model_name e3d_lstm \
-    --num_hidden 4,4 \
-    --filter_size 2 \
+    --num_hidden 8,8,4 \
+    --filter_size 10 \
     --layer_norm True \
     --scheduled_sampling True \
     --sampling_stop_iter 40 \
     --sampling_start_value 1.0 \
     --sampling_delta_per_iter 0.00002 \
-    --lr 0.001 \
-    --batch_size 2 \
-    --max_iterations 5 \
+    --lr 0.1 \
+    --batch_size 8 \
+    --max_iterations 200 \
     --display_interval 1 \
     --test_interval 1 \
-    --snapshot_interval 5 \
+    --snapshot_interval 200 \
     --allow_gpu_growth True
